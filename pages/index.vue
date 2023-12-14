80797cde52a81265c2b7ccc0d9a75796970d64af
Guilherme200 <guimatoso15@gmail.com>
2023-12-14 00:47:34 -0300

    Configurar layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,55 @@
 <template>
-  <div>
-      <h1 class="text-3xl text-cyan-500">
-        De um passo para investir
-      </h1>
-  </div>
+  <section class="bg-gray-100">
+    <div class="grid grid-cols-5 h-screen">
+      <div class="navbar bg-base-100 col-span-3 items-start">
+        <div class="navbar-start">
+          <img class="w-24" src="https://logosmarcas.net/wp-content/uploads/2020/04/Nike-Logo.png" alt="logo">
+        </div>
+        <div class="navbar-center">
+          <ul class="flex gap-10 font-medium">
+            <li class="active">
+              <nuxt-link to="/"/>
+              Início
+            </li>
+
+            <li>
+              <nuxt-link to="/"/>
+              Cadastro
+            </li>
+
+            <li>
+              <nuxt-link to="/"/>
+              Quem somos
+            </li>
+
+            <li>
+              <nuxt-link to="/"/>
+              POD
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="bg-secondary col-span-2">
+        dedewdew
+      </div>
+    </div>
+
+
+  </section>
 </template>
+
+<style scoped>
+
+</style>
+<script setup lang="ts">
+</script>
+
+<style scoped>
+nav > ul > li {
+  @apply link no-underline hover:underline text-gray-500 font-bold;
+}
+
+nav > ul > .active {
+  @apply text-secondary;
+}
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,31 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
-  theme: {
-    extend: {
-      fontFamily: {
-        'morganite': 'Morganite-Medium',
-      },
-      colors: {
-        'dark': '#140438',
-        'purple': '#9503EC',
-        'blue': '#008CF1',
-        'cyan': '#00CEFF'
-      },
+import daisyui from 'daisyui';
+
+module.exports = {
+  config: {
+    content: [
+      './components/**/*.{js,vue,ts}',
+      './layouts/**/*.vue',
+      './pages/**/*.vue',
+      './plugins/**/*.{js,ts}',
+      './nuxt.config.{js,ts}',
+    ],
+    plugins: [(daisyui)],
+    daisyui: {
+      themes: [
+        {
+          mytheme: {
+            'primary': '#140438',
+            'secondary': '#9503EC',
+            'accent': '#00CEFF',
+            'neutral': '#008CF1',
+            'base-100': '#ffffff',
+            'info': '#00f9ff',
+            'success': '#00992f',
+            'warning': '#ff9b00',
+            'error': '#ff3557',
+          },
+        },
+      ],
     },
   },
-  plugins: [],
 }
-
