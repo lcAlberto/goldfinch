6f65792c07cedf78ad35be94641b075d6bc888df
Guilherme200 <guimatoso15@gmail.com>
2023-12-14 12:09:52 -0300

    sessão home

--- pages/index.vue
+++ pages/index.vue
@@ -1,41 +1,27 @@
 <template>
-  <section class="bg-gray-100">
-    <div class="grid grid-cols-5 h-screen">
-      <div class="navbar bg-base-100 col-span-3 items-start">
-        <div class="navbar-start">
-          <img class="w-24" src="https://logosmarcas.net/wp-content/uploads/2020/04/Nike-Logo.png" alt="logo">
+  <div class="fixed navbar bg-primary z-10 p-16"></div>
+  <section class="w-full relative flex bg-gray-100 h-screen overflow-hidden">
+    <div class="w-[60%] flex justify-start mt-[20%]">
+      <div class="w-1/3 ml-[30%] block">
+        <h1 class="text-5xl font leading-[3.5rem]">
+          Dê um passo para
+          <span class="text-secondary font-bold">investir</span>
+          no futuro
+        </h1>
+        <h4 class="text-lg mt-16 text-gray-400">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+        </h4>
+        <div class="mt-16">
+          <button class="btn btn-sm btn-secondary px-5 uppercase tracking-[8px] rounded-3xl">Conheça</button>
         </div>
-        <div class="navbar-center">
-          <ul class="flex gap-10 font-medium">
-            <li class="active">
-              <nuxt-link to="/"/>
-              Início
-            </li>
-
-            <li>
-              <nuxt-link to="/"/>
-              Cadastro
-            </li>
-
-            <li>
-              <nuxt-link to="/"/>
-              Quem somos
-            </li>
-
-            <li>
-              <nuxt-link to="/"/>
-              POD
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div class="bg-secondary col-span-2">
-        dedewdew
       </div>
     </div>
-
-
+    <div class="w-[40%] bg-[url('~/assets/images/background/home.png')]">
+      <img class="w-1/2 z-1 absolute right-[15%] mt-10 mr-24" src="~/assets/images/background/01.png" alt="01">
+    </div>
   </section>
+
+  <section class="relative w-full flex bg-gray-100 h-screen overflow-hidden"></section>
 </template>
 
 <style scoped>
