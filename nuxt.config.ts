c65cc147f3cbc525d0f11baf6766827f9df67f7a
lcAlberto <lcalbertomatoso2012@gmail.com>
2024-02-04 17:07:49 -0300

    Adicionar favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      link: [
+        { rel: "icon", type: "image/png", href: "/favicon.ico" }
+      ]
+    }
+  },
   ssr: false,
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
